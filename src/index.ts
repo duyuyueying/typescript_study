a80cf10d332a475dbbaf2861cdd720395a90932b
王小玉 <1551206366@qq.com>
2021-09-27 22:54:50 +0800

    feat: axios xhr方法实现

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { axiosRequestConfig } from './types'
+import xhr from './xhr'
+function axios(config: axiosRequestConfig): void {
+  xhr(config)
+}
+
+export default axios
---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'delete'
+  | 'DELETE'
+  | 'put'
+  | 'PUT'
+  | 'options'
+  | 'OPTIONS'
+  | 'head'
+  | 'Head'
+  | 'patch'
+  | 'PATCH'
+
+export interface axiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
