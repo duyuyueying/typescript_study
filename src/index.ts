ee803eee1e5db04473298c9ad8d260eb64c8a894
王小玉 <1551206366@qq.com>
2021-10-17 22:02:45 +0800

    feat: error

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,4 @@
-import { transformResponse, transfromRequest } from './helpers/data'
-import { processHeaders } from './helpers/headers'
-import { buildURL } from './helpers/url'
-import { AxiosPromise, axiosRequestConfig } from './types'
-import xhr from './xhr'
-function axios(config: axiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then((res) => {
-    res.data = transformResponse(res.data)
-    return res 
-  })
-}
+import axios from './axios'
 
-function processConfig(config: axiosRequestConfig): void {
-  config.url = transformURL(config)
-  config.headers = transfromHeaders(config)
-  config.data = transfromRequestData(config)
-}
-
-function transformURL(config: axiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-function transfromRequestData(config: axiosRequestConfig): any {
-  const { data } = config
-  return transfromRequest(data)
-}
-
-function transfromHeaders(config: axiosRequestConfig): any {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
+export * from './types'
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,17 +1,25 @@
 import { resolve } from 'path'
+import { createError } from './helpers/error'
 import { parseHeaders } from './helpers/headers'
 import { axiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 
 export default function xhr(config: axiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { data = null, url, method = 'get', headers, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { data = null, url, method = 'get', headers, responseType, timeout } = config
     const request = new XMLHttpRequest()
     if (responseType) {
       request.responseType = responseType
     }
+    if (timeout) {
+      request.timeout = timeout
+    }
     request.open(method.toUpperCase(), url, true)
     request.onreadystatechange = function handledLoad() {
       if (request.readyState !== 4) {
+        return
+      }
+      // 当网络错误或者超时的时候status也为0,所以直接return即可
+      if (request.status === 0) {
         return
       }
       const responseHeaders = parseHeaders(request.getAllResponseHeaders())
@@ -24,8 +32,15 @@ export default function xhr(config: axiosRequestConfig): AxiosPromise {
         config,
         request
       }
-      resolve(response)
+      handleResponse(response)
     }
+    request.onerror = function handleError() {
+      reject(createError('Network Error', config, null, request))
+    }
+    request.ontimeout = function handleTimeout() {
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
+    }
+
     Object.keys(headers).forEach(name => {
       // data为null的时候设置content-type是没有意义的，所以可以不用设置
       if (data === null && name.toLowerCase() == 'content-type') {
@@ -35,5 +50,21 @@ export default function xhr(config: axiosRequestConfig): AxiosPromise {
       }
     })
     request.send(data)
+
+    function handleResponse(response: AxiosResponse): void {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(
+          createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
+      }
+    }
   })
 }
